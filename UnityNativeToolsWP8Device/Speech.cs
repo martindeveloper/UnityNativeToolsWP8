2ecebbe32e378ae0d541ed0bbeac41a06ee41702
Martin Pernica <pernica@pria.cz>
2014-08-12 16:09:30 +0200

    First version of Unity WP8 tools

---
+++ UnityNativeToolsWP8Device/Speech.cs
@@ -0,0 +1,13 @@
+﻿using Windows.Phone.Speech.Synthesis;
+
+namespace UnityNativeToolsWP8
+{
+    public class Speech
+    {
+        public static void SayText(string text)
+        {
+            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+            synthesizer.SpeakTextAsync(text);
+        }
+    }
+}
---
+++ UnityNativeToolsWP8Editor/Speech.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnityNativeToolsWP8
+{
+    public class Speech
+    {
+        public static void SayText(string text)
+        {
+            Console.WriteLine("UnityNativeToolsWP8.Speech.SayText - " + text);
+        }
+    }
+}
